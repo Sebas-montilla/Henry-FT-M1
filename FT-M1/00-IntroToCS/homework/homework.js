0181c45fc39ebded2df9459769749e4079c8bb1a
Sebas_montilla <sebastianam127@gmail.com>
2022-04-11 12:57:56 -0400

    Test

--- FT-M1/00-IntroToCS/homework/homework.js
+++ FT-M1/00-IntroToCS/homework/homework.js
@@ -2,14 +2,25 @@
 
 function BinarioADecimal(num) {
   // tu codigo aca
-
+  let decimal = 0;
+  let result;
+  let newNum = num.split('').reverse();
+  for (let i = 0; i < newNum.length; i++) {
+    result = (newNum[i]*Math.pow(2,i));
+    decimal = result + decimal
+  }
+  return decimal
 }
 
 function DecimalABinario(num) {
   // tu codigo aca
-
+  let binaries = [];
+  do {
+    binaries.push(num % 2)
+    num = Math.floor(num / 2);
+  } while (num !== 0);
+  return binaries.reverse().join('')
 }
-
 
 module.exports = {
   BinarioADecimal,
